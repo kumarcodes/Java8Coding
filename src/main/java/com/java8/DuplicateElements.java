3c05cfe088cee959f44c3bf8dbd16d8aa47255e4
Rohit Sharma <merohitks@gmail.com>
2026-04-19 01:43:16 +0530

    Add DuplicateElements class to identify and print duplicate elements from a list using streams

---
+++ src/main/java/com/java8/DuplicateElements.java
@@ -0,0 +1,16 @@
+package com.java8;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DuplicateElements {
+    public static void main(String[] args) {
+        List<Integer> myList = Arrays.asList(10, 15, 8, 49, 25, 98, 98, 32,
+                15);
+        Set<Integer> uniqueSet = new HashSet<>();
+        myList.stream().filter(x->!uniqueSet.add(x)).forEach(System.out::println);
+
+    }
+}
