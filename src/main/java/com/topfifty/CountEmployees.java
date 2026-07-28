21a711d228ad131e5c80834de11a783086314f6d
Rohit Sharma <merohitks@gmail.com>
2026-07-29 00:20:21 +0530

    Add CountEmployees class to count employees by department

--- src/main/java/com/topfifty/CountEmployees.java
+++ src/main/java/com/topfifty/CountEmployees.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 public class CountEmployees {
     public static void main(String[] args) {
         List<Employees> employees = EmployeesDTO.getEmployees();
+
         employees.stream()
                 .collect(Collectors.groupingBy(Employees::getDepartment, Collectors.counting()))
                 .entrySet().forEach(System.out::println);
