012df35e45c88be8937e6720a17012b51b95986d
Rohit Sharma <merohitks@gmail.com>
2026-07-29 21:04:30 +0530

    Add PatternPrint class to print a right-angled triangle pattern of asterisks

---
+++ src/main/java/com/corejava/PatternPrint.java
@@ -0,0 +1,14 @@
+package com.corejava;
+
+public class PatternPrint {
+    public static void main(String[] args) {
+        int rows = 5;
+        int col = 5;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
