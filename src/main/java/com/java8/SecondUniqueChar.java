42b3ee0d5a44198b2b462b75ef0bac3da2478575
Rohit Sharma <merohitks@gmail.com>
2026-08-05 11:55:40 +0530

    Add SecondUniqueChar class to find the second unique character in a string

---
+++ src/main/java/com/java8/SecondUniqueChar.java
@@ -0,0 +1,18 @@
+package com.java8;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class SecondUniqueChar {
+    public static void main(String[] args) {
+        String str = "rohit kumar sharma";
+        String secondUniqueChar = Arrays.stream(str.replace(" ", "").split(""))
+                .collect(Collectors.groupingBy(x -> x, LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream().filter(x -> x.getValue() == 1)
+                .map(x -> x.getKey()).skip(1).findFirst().map(String::valueOf).orElse("No second unique character found");
+
+        System.out.println(secondUniqueChar);
+    }
+}
