0dc99e216bd78b8ce1f0445edb5f088ec68d6194
Rohit Sharma <merohitks@gmail.com>
2026-08-09 00:03:35 +0530

    Add WordLengthCounter class to filter and print short words

---
+++ src/main/java/com/java8/WordLengthCounter.java
@@ -0,0 +1,11 @@
+package com.java8;
+
+import java.util.Arrays;
+
+public class WordLengthCounter {
+    public static void main(String[] args) {
+        String str = "rohit kumar sharma rks dm";
+        Arrays.stream(str.split(" ")).map(String::toUpperCase).filter(x -> x.length() < 4)
+                .forEach(System.out::println);
+    }
+}
