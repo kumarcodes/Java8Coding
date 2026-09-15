1994eea9c1f803615cc4f4633226cfdf32faedba
Rohit Sharma <merohitks@gmail.com>
2026-09-16 00:59:08 +0530

    Add OddEvenThread class to print odd and even numbers using multithreading

---
+++ src/main/java/com/multithreading/OddEvenThread.java
@@ -0,0 +1,32 @@
+package com.multithreading;
+
+//Print numbers from 1 to 30 in order.
+// Odd numbers and Even Numbers should be printed into two different threads.
+public class OddEvenThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 1; i <= 29; i = i + 2) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException ignored) {
+                }
+
+            }
+
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 2; i <= 30; i = i + 2) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException ignored) {
+
+                }
+            }
+        });
+        t1.start();
+        Thread.sleep(2000);
+        t2.start();
+    }
+}
