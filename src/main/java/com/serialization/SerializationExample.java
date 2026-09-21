ce1b6ec466850b157bea95528ce7645305e96a80
Rohit Sharma <merohitks@gmail.com>
2026-09-21 23:26:53 +0530

    Add RestTemplateExample class and update SerializationExample with stream operations

--- src/main/java/com/serialization/SerializationExample.java
+++ src/main/java/com/serialization/SerializationExample.java
@@ -1,6 +1,10 @@
 package com.serialization;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class User implements Serializable {
 
@@ -25,13 +29,23 @@ class User implements Serializable {
 public class SerializationExample {
 
     public static void main(String args[]) throws FileNotFoundException {
-        User user = new User("rohit", "password");
-        String filename = "user.ser";
-        try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(filename))) {
-            output.writeObject(user);
-            System.out.println("Serialization successful");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        User user = new User("rohit", "password");
+//        String filename = "user.ser";
+//        try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(filename))) {
+//            output.writeObject(user);
+//            System.out.println("Serialization successful");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        list.stream().peek(System.out::println);
+        //System.out.println(count);
+       // First Non-Repeated Character
+        String str = "Rohit Kumar Sharma";
+        Arrays.stream(str.split("")).collect(Collectors.groupingBy(x->x, LinkedHashMap::new, Collectors.counting()))
+                .forEach((x,y)->{
+                    System.out.println(x+" "+y);
+                });
+        //Query to
     }
 }
---
+++ src/main/java/com/synchronus/RestTemplateExample.java
@@ -0,0 +1,24 @@
+package com.synchronus;
+
+public class RestTemplateExample {
+    /*
+    @Service
+    public class OrderService
+
+    {
+
+        private final String url = "https://inventory-service/api/inventory";
+        private final RestTemplate restTemplate;
+
+        public String placeOrder(String OrderRequest order, String authToken)
+        {
+            HttpHeaders headers = new HtttpHeaders;
+            headers.setContentType();
+            headers.setAccept();
+            headers.set(token);
+
+            ResponseEntity<Inventory> response = restTemplate.exchange(url, HTTPMethod.POST, OrderRequest);
+            return "order-placed successfully";
+        }
+*/
+}
